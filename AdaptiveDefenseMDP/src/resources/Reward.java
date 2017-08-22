051bd21652cdc92dddfb40b7304e66ede22aff2a
Yehia <yehia.elrakaiby@gmail.com>
2017-08-22 16:33:59 +0100

    add hashcode and equals to Reward class

--- AdaptiveDefenseMDP/src/resources/Reward.java
+++ AdaptiveDefenseMDP/src/resources/Reward.java
@@ -43,6 +43,37 @@ public class Reward {
 	public void updateReward(Integer reward) {
 		this.reward += reward;
 	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((dest == null) ? 0 : dest.hashCode());
+		result = prime * result + ((src == null) ? 0 : src.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Reward other = (Reward) obj;
+		if (dest == null) {
+			if (other.dest != null)
+				return false;
+		} else if (!dest.equals(other.dest))
+			return false;
+		if (src == null) {
+			if (other.src != null)
+				return false;
+		} else if (!src.equals(other.src))
+			return false;
+		return true;
+	}
 	
 
 }
